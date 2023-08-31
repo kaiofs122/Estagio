77e304c44986e9b271250d547280fd085f399056
Kaio Fernandes <kaiofs122@gmail.com>
2023-08-31 16:23:18 -0300

    aprendendo sobre rotas

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,45 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('admin.clientes');
 });
 
-Route::get('/empresa', function(){
-    return view('empresa');
+Route::any('/any', function(){
+    return "Permite todo tipo de acesso http (put, delete, get, post)";
+});
+
+Route::match(['get', 'post'], '/match', function(){
+    return'Permite apenas acessos definidos';
+});
+
+Route::get('/produto/{id}/{cat?}', function($id, $cat =""){
+    return "O id do produto é: ".$id. "<br>". "A categoria é: ". $cat;
+});
+
+route::redirect('/sobre', '/empresa');
+
+Route::view('/empresa','site/empresa');
+
+Route::get('/timesnownews', function(){
+    return view('news');
+})->name('noticias');
+
+Route::get('/novidades', function(){
+    return redirect()->route('noticias');
+});
+ 
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function(){
+
+    Route::get('dashboard', function(){
+        return "dashboard";
+    })->name('dashboard');
+
+    Route::get('users', function(){
+        return "users";
+    })->name('users');
+
+    Route::get('clientes', function(){
+        return "Clientes";
+    })->name('clientes');
+
 });
