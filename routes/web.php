34a473627881cc132d580586bddb6a4919e3a66b
Kaio Fernandes <kaiofs122@gmail.com>
2023-09-01 10:03:20 -0300

    criando migrations

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
 
-
-Route::get('/', [ProdutoController::class, 'index'])->name('produto.index');
-
-Route::get('/produto/{id?}', [ProdutoController::class, 'interessante'])->name('produto.show');
-
+Route::resource('produtos', ProdutoController::class);
