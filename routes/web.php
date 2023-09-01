2fbd0eff24f0e9811b24bcfa6f695648b4566a3a
Kaio Fernandes <kaiofs122@gmail.com>
2023-09-01 09:22:22 -0300

    Testanto controllers

--- routes/web.php
+++ routes/web.php
@@ -1,58 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdutoController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return redirect()->route('admin.clientes');
-});
+Route::get('/', [ProdutoController::class, 'index']);
 
-Route::any('/any', function(){
-    return "Permite todo tipo de acesso http (put, delete, get, post)";
-});
 
-Route::match(['get', 'post'], '/match', function(){
-    return'Permite apenas acessos definidos';
-});
-
-Route::get('/produto/{id}/{cat?}', function($id, $cat =""){
-    return "O id do produto é: ".$id. "<br>". "A categoria é: ". $cat;
-});
-
-route::redirect('/sobre', '/empresa');
-
-Route::view('/empresa','site/empresa');
-
-Route::get('/timesnownews', function(){
-    return view('news');
-})->name('noticias');
-
-Route::get('/novidades', function(){
-    return redirect()->route('noticias');
-});
- 
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function(){
-
-    Route::get('dashboard', function(){
-        return "dashboard";
-    })->name('dashboard');
-
-    Route::get('users', function(){
-        return "users";
-    })->name('users');
-
-    Route::get('clientes', function(){
-        return "Clientes";
-    })->name('clientes');
-
-});
