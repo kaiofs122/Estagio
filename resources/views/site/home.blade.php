2f821a1bd2c4fa5079cb4b1222a081a304c3818e
Kaio Fernandes <kaiofs122@gmail.com>
2023-09-04 07:18:20 -0300

    estruturas de seleção

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -2,4 +2,18 @@
 @section('title', 'Essa é a página HOME')
 @section('conteudo')
     <h1> Essa é a nossa Home </h1>
+
+{{--Isso é um comentário--}}    
+
+{{-- isset($nome) ? 'existe' : 'não existe'--}}
+
+{{$teste ?? 'Padrao'}}
+
+{{--Estrutura de controle--}}
+
+@if($nome == 'Kaio')
+    true
+@endif
+    false
+
 @endsection
